4b37e4fa6c26e77673b832a79a1be0ef1b46d3c2
wswendisson5-droid <wswendisson5@gmail.com>
2026-09-18 22:21:28 -0300

    ops: restart Passenger with background sync disabled

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 // Setup Node.js App (cPanel / Passenger) Entrypoint
-// Passenger bootstrap: restart marker 2026-09-18T19:59-03:00. Compiled backend is dist/server.cjs.
+// Passenger bootstrap: restart marker 2026-09-18T22:21-03:00. Compiled backend is dist/server.cjs.
 import './dist/server.cjs';
